380b437631d6bc1643ab94782686f5b5b70c1b6d
Mauricio Martinez <MaodeColombia@users.noreply.github.com>
2020-03-29 23:51:08 -0500

    Update SSH_connection.py

--- SSH_connection.py
+++ SSH_connection.py
@@ -8,6 +8,7 @@ import paramiko
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#falta <I.P.>, <>, <>
 ssh_client.connect('<I.P.>', port=22, username='<>', password='<>',timeout=4)
 
 entrada, salida, error = ssh_client.exec_command('show signaling sccp destination-point-code all')
